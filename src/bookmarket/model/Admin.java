841794add16c1c9034a6e8f3efdb83e72236450e
hyejeong1019 <hyejeong1019@kopo.ac.kr>
2024-05-30 20:28:45 +0900

    Admin 클래스, booklist 추가

---
+++ src/bookmarket/model/Admin.java
@@ -0,0 +1,14 @@
+package bookmarket.model;
+
+public class Admin extends User {
+	private String id = "admin";
+	private String password = "1234";
+	
+	public boolean login(String id, String password) {
+		if (!this.id.equals(id)) return false;
+		if (!this.password.equals(password)) return false;
+		
+		return true;
+	}
+	
+}
